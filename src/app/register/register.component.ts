37134a29359620f6e00b922dc700e3769225d1aa
Hamzaba92 <153004965+Hamzaba92@users.noreply.github.com>
2024-09-18 15:04:45 +0200

    finish sign-up component

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,12 +1,64 @@
 import { Component } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ErrorStateMatcher } from '@angular/material/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import {
+  FormControl,
+  FormGroupDirective,
+  NgForm,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
+
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, CommonModule, ReactiveFormsModule, MatIconModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+  passwordFormControl = new FormControl('', [
+    Validators.required,
+    Validators.minLength(8),
+    Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)]);
+
+
+  confirmPasswordFormControl = new FormControl('', [Validators.required]);
+  matcher = new MyErrorStateMatcher();
+
+  get passwordsMatch(): boolean {
+    return this.passwordFormControl.value === this.confirmPasswordFormControl.value;
+  }
+
+  firstName = '';
+  lastName = '';
+  email = '';
+
+
+  hidePassword = true;
+  hideConfirmPassword = true;
+
+
+  togglePasswordVisibility(type: string): void {
+    if (type === 'password') {
+      this.hidePassword = !this.hidePassword;
+    } else if (type === 'confirmPassword') {
+      this.hideConfirmPassword = !this.hideConfirmPassword;
+    }
+  }
 
 }
