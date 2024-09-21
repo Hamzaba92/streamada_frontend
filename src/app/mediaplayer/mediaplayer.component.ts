b2996451c00eff448b6915ea06fd8c6640ca3be8
Hamzaba92 <153004965+Hamzaba92@users.noreply.github.com>
2024-09-22 00:27:52 +0200

    implement c mediaplayer with background und dropdown-menu

--- src/app/mediaplayer/mediaplayer.component.ts
+++ src/app/mediaplayer/mediaplayer.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-mediaplayer',
   standalone: true,
-  imports: [],
+  imports: [NgbDropdownModule],
   templateUrl: './mediaplayer.component.html',
   styleUrl: './mediaplayer.component.scss'
 })
 export class MediaplayerComponent {
+
+  constructor(private router: Router){}
+
+  currentVideoSrc: string = 'assets/videos/test_video.mp4';
+
+  backToOverview() {
+    this.router.navigate(['streamada-overview']);
+  }
 
 }
