9be553692c2b27b386df17ec08e54f64c9631d3f
Hamzaba92 <153004965+Hamzaba92@users.noreply.github.com>
2024-09-25 02:49:43 +0200

    add spinner and success-banner in forget-your-pw-comp

--- src/app/forget-password/forget-password.component.ts
+++ src/app/forget-password/forget-password.component.ts
@@ -1,6 +1,6 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, formatDate } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { Router } from '@angular/router';
@@ -15,10 +15,36 @@ import { Router } from '@angular/router';
 export class ForgetPasswordComponent {
   email = '';
 
+  loading: boolean = false;
+  showpopup: boolean = false;
+
   constructor(private router: Router) { }
 
   goToLoginComp() {
     this.router.navigate(['login']);
   }
 
+  closePopup() {
+    this.showpopup = false;
+    this.email = '';
+  }
+
+
+  onSubmit() {
+    if (this.email) {
+
+      console.log('send');
+
+      this.loading = true;
+
+      setTimeout(() => {
+        this.loading = false;
+        this.showpopup = true;
+      }, 4000);
+      
+
+
+
+    }
+  }
 }
